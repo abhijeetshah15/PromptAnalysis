fd17c9cddb47702f1df67479af09a9cca289f382
Abhijeet Shah <shahabh@oregonstate.edu>
2024-02-28 14:00:07 -0800

    Updated the ML Files
    Updated the test script for ML Model (added predicted results csv as output).
    
    Added more test data.
    
    Removed the train data from ML folder as it is already in Sample Data.

--- ML/main.py
+++ ML/main.py
@@ -4,11 +4,11 @@ from joblib import load
 from sklearn.metrics import accuracy_score, classification_report
 
 # Load Model and Vectorizer
-model = load('RandomForestClassifier.joblib')
-tfidf_vectorizer = load('tfidf_vectorizer.joblib')
+model = load('ML/RandomForestClassifier.joblib')
+tfidf_vectorizer = load('ML/tfidf_vectorizer.joblib')
 
 # Read all CSV files in the current directory
-csv_files = glob.glob('./*.csv')
+csv_files = glob.glob('./Test Data/*.csv')
 all_data = pd.DataFrame()
 for csv_file in csv_files:
     data = pd.read_csv(csv_file)
@@ -23,6 +23,16 @@ y_true = fdata['Solved/Unsolved'].apply(lambda x: 1 if x == 'Solved' else 0)
 
 # Evaluation model
 y_pred = model.predict(X)
+
+# Create a DataFrame with original prompts, original 'Solved/Unsolved', and predicted 'Solved/Unsolved'
+results_df = pd.DataFrame({
+    'Prompts': fdata['Prompts'],
+    'Original_Solved/Unsolved': fdata['Solved/Unsolved'],
+    'Predicted_Solved/Unsolved': ['Solved' if pred == 1 else 'Unsolved' for pred in y_pred]
+})
+
+# Save the DataFrame to a CSV file
+results_df.to_csv('ML Model Results/predicted_results.csv', index=False)
 
 # Print results
 print("Evaluating combined predictions")
